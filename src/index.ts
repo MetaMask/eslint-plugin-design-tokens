e8801bb144825cc909571855041c8eb53f184409
georgewrmarshall <george.marshall@consensys.net>
2024-03-26 17:55:48 -0700

    updating dependencies:

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-import type { TSESLint } from '@typescript-eslint/experimental-utils';
-
 import { rules } from './rules';
 
-type RuleKey = keyof typeof rules;
-
-type Plugin = {
-  rules: Record<RuleKey, TSESLint.RuleModule<any, any, any>>;
-} & Omit<TSESLint.Plugin, 'rules'>;
-
-const plugin: Plugin = {
-  // Assuming you want to add metadata here, but ESLint plugins defined in JS/TS typically don't include a 'meta' field at the plugin level.
-  // 'meta' is usually part of individual rule definitions.
+const plugin = {
   rules,
 };
 
